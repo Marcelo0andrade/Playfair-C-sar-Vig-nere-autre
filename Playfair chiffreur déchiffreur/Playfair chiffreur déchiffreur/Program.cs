b6c22f412d1edb4daff41e38899935f774fa2d25
Marcelo Andrade <marcelo.dasilvaandrade@rpn.ch>
2017-12-07 13:42:00 +0100

    add entrer and exit + fonctions

--- Playfair chiffreur déchiffreur/Playfair chiffreur déchiffreur/Program.cs
+++ Playfair chiffreur déchiffreur/Playfair chiffreur déchiffreur/Program.cs
@@ -8,8 +8,44 @@ namespace Playfair_chiffreur_déchiffreur
 {
     class Program
     {
+        string stralpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        string strMotclef;
+        string strfinal;
+        char clettre;
+        int istart = 0;
+
         static void Main(string[] args)
         {
+            do
+            {
+                Console.WriteLine("Saisissez: \n ( 1 ) pour chiffrer \n ( 2 ) pour déchiffrer");
+                string strsaisie = Console.ReadLine();
+                int ichiffreroupas = Convert.ToInt32(strsaisie);
+                switch (ichiffreroupas)
+                {
+                    case 1:
+                        chiffrer();
+                        break;
+                    case 2:
+                        déchiffrer();
+                        break;
+                }
+                Console.WriteLine("Voulez vous quitter l'application ? (o/n)");
+                char cpartir = Console.ReadKey().KeyChar;
+                if (cpartir == 'o'){
+                    Console.WriteLine("Merci de la visite");
+                }
+            } while (true);
+        }
+
+        static void chiffrer()
+        {
+            Console.WriteLine("Chiffrer");
+
+        }
+        static void déchiffrer()
+        {
+            Console.WriteLine("Déchiffrer");
         }
     }
 }
